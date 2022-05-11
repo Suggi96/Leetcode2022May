45767a7063816a09cabe6669b6441fee1dd5bc63
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-05-11 14:34:05 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.06%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,18 +1,15 @@
 class Solution {
     public int countVowelStrings(int n) {
-        Integer[][] memo = new Integer[n + 1][5];
-        return dp(n, 0, memo);
-    }
-
-    int dp(int n, int i, Integer memo[][]) {
-        if (n == 0) return 1; // Found a valid answer
-        if (i == 5) return 0; // Reach to length of vowels [a, e, i, o, u]
-        if (memo[n][i] != null) return memo[n][i];
-        
-        int pickCurChar = dp(n, i+1, memo);
-        int skipCurChar = dp(n-1, i, memo);
-        int ans = pickCurChar + skipCurChar;
-        memo[n][i] = ans;
-        return memo[n][i];
+        int[] dp = new int[5];
+        Arrays.fill(dp, 1);
+        for(int i=2;i<=n;i++) {
+            for(int j=3;j>=0;j--) {
+                dp[j] = dp[j] + dp[j+1];
+            }
+        }
+        int count = 0;
+        for(int i: dp)
+            count += i;
+        return count;
     }
 }
