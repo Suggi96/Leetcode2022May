2939da7c35756f50b28f4b311a01be194b8f091b
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-05-15 11:47:51 +0530

    Time: 10 ms (29.91%), Space: 58.9 MB (31.36%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -15,17 +15,15 @@
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        int h = findHeight(root);
+   //     int h = findHeight(root);
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        int level = 0;
         int sum = 0;
         while(!q.isEmpty()) {
-            level++;
+            sum = 0;
             int size = q.size();
             for(int i=0;i<size;i++) {
                 TreeNode curNode = q.remove();
-                if(curNode.left==null && curNode.right==null && level==h)
                     sum += curNode.val;
                 if(curNode.left!=null)
                     q.add(curNode.left);
@@ -35,12 +33,5 @@ class Solution {
         }
         return sum;
         
-    }
-    private int findHeight(TreeNode root) {
-        if(root==null)
-            return 0;
-        int leftH = findHeight(root.left);
-        int rightH = findHeight(root.right);
-        return 1 + Math.max(leftH, rightH);
     }
 }
