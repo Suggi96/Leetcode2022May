d1b7fa90e0080d9232f7ace29f41941243e3ddaf
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-05-19 15:01:27 +0530

    Time: 13 ms (47.41%), Space: 54.4 MB (31.88%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,30 +1,29 @@
 class Solution {
-public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-public int longestIncreasingPath(int[][] matrix) {
-    if(matrix.length == 0) return 0;
-    int m = matrix.length, n = matrix[0].length;
-    int[][] cache = new int[m][n];
-    int max = 1;
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            int len = dfs(matrix, i, j, m, n, cache);
-            max = Math.max(max, len);
+    int[][] dir = {{-1,0}, {1,0}, {0,1}, {0,-1}};
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix==null) return 0;
+        int m = matrix.length, n = matrix[0].length;
+        int longestPath = 0;
+        int[][] memo = new int[m][n];
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                int path = dfs(matrix, m, n, i, j, memo);
+                longestPath = Math.max(longestPath, path);
+            }
         }
-    }   
-    return max;
-}
-
-public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
-    if(cache[i][j] != 0) return cache[i][j];
-    int max = 1;
-    for(int[] dir: dirs) {
-        int x = i + dir[0], y = j + dir[1];
-        if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
-        int len = 1 + dfs(matrix, x, y, m, n, cache);
-        max = Math.max(max, len);
+        return longestPath;
     }
-    cache[i][j] = max;
-    return max;
-}
+    private int dfs(int[][] matrix, int m, int n, int i, int j, int[][] memo) {
+        if(memo[i][j]>0)
+            return memo[i][j];
+        int len = 0;
+        for(int[] d: dir) {
+            int x = i + d[0];
+            int y = j + d[1];
+            if(x>=0 && y>=0 && x<m && y<n && matrix[x][y]>matrix[i][j])
+                len = Math.max(len, dfs(matrix, m, n, x, y, memo));
+        }
+        memo[i][j] = len+1;
+        return len+1;
+    }
 }
