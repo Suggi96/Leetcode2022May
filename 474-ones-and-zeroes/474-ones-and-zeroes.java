4cfa558a7d51e4182c993c69bbc26905800dde51
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-05-23 09:48:12 +0530

    Time: 1826 ms (5.03%), Space: 404.6 MB (5.03%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        return helper(strs, m, n, 0, new HashMap<String, Integer>());
+    }
+    private int helper(String[] strs, int zero, int one, int index, HashMap<String, Integer> memo) {
+        if(index==strs.length || zero+one==0)
+            return 0;
+        String key = zero + "" + "-" + "" + one + "-" + "" + index;
+        if(memo.containsKey(key))
+            return memo.get(key);
+        int[] count = countZO(strs[index]);
+        int consider = 0;
+        if(zero>=count[0] && one>=count[1])
+            consider = 1 + helper(strs, zero-count[0], one-count[1], index+1, memo);
+        int notConsider = helper(strs, zero, one, index+1, memo);
+        
+        int ans = Math.max(consider, notConsider);
+        memo.put(key, ans);
+        return memo.get(key);
+    }
+    private int[] countZO(String s) {
+        int count[] = new int[2];
+        for(char c: s.toCharArray()) {
+            count[c - '0']++;
+        }
+        return count;
+    }
+}
