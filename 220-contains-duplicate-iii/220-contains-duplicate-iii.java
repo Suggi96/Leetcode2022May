bbff7b5e47738795fd77ae4220cb72e5a2bd7bc2
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-08 22:33:21 +0530

    Time: 57 ms (46.05%), Space: 52.7 MB (70.31%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.java
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-    if(nums == null || nums.length == 0 || k <= 0 || t < 0) {
+        if(nums.length==0 || k<0 || t<0)
             return false;
-        }
         TreeSet<Long> set = new TreeSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            long num = (long) nums[i];
-            Long floor = set.floor(num);
-            if (floor != null && num - floor <= t) {
+        //floor -> <=
+        //ceiling -> >=
+        for(int i=0;i<nums.length;i++) {
+            Long el = new Long(nums[i]);
+            Long floor = set.floor(el);
+            if(floor!=null && Math.abs(floor - el) <=t)
                 return true;
-            }
-            Long ceiling = set.ceiling(num);
-            if (ceiling != null && ceiling - num <= t) {
+            Long ceil = set.ceiling(el);
+            if(ceil!=null && Math.abs(ceil - el) <=t)
                 return true;
-            }
-            set.add(num);
-            if (i >= k) {
-                set.remove((long) nums[i - k]);
-            }
+            set.add(el);
+            if(i>=k)
+                set.remove(new Long(nums[i-k]));
         }
-        return false; 
+        return false;
+        
     }
 }
