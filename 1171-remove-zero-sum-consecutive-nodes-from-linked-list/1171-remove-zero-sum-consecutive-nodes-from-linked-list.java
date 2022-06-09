18bb10ff6ed00b683b432168aa4d44ddf555f7f3
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-09 11:32:45 +0530

    Time: 4 ms (49.90%), Space: 44.2 MB (45.74%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -9,32 +9,23 @@
  * }
  */
 class Solution {
-    public ListNode removeZeroSumSublists(ListNode head) {
-        if(head==null)
-            return null;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        int prefixSum = 0;
-        HashMap<Integer, ListNode> map = new HashMap<>();
-        map.put(prefixSum, dummy);
-        while(head!=null) {
-            prefixSum += head.val;
-            if(map.containsKey(prefixSum)) {
-                ListNode start = map.get(prefixSum);
-                int sum = prefixSum;
-                while(start!=null && start!=head) {
-                    start = start.next;
-                    sum += start.val;
-                    if(start!=head)
-                        map.remove(sum);
-                }
-                map.get(prefixSum).next = head.next; //connect next node of prefix sum to next node of head in current iteration
-            }
-            else {
-                map.put(prefixSum, head);
-            }
-            head = head.next;
-        }
-        return dummy.next;
+public static ListNode removeZeroSumSublists(ListNode head) {
+    int prefixSum = 0;
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+
+    Map<Integer, ListNode> hashMap = new HashMap<>();
+    hashMap.put(0, dummy);
+    for (ListNode i = dummy; i != null; i = i.next) {
+        prefixSum += i.val;
+        hashMap.put(prefixSum, i);
     }
+    prefixSum = 0;
+    for (ListNode i = dummy; i != null; i = i.next) {
+        prefixSum += i.val;
+        i.next = hashMap.get(prefixSum).next;
+    }
+    return dummy.next;
+}
+    
 }
