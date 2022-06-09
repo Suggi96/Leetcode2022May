f685d6c739637e2b829546c429339225244ea144
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-09 19:45:49 +0530

    Time: 5 ms (32.64%), Space: 45.2 MB (7.90%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -9,23 +9,21 @@
  * }
  */
 class Solution {
-public static ListNode removeZeroSumSublists(ListNode head) {
-    int prefixSum = 0;
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-
-    Map<Integer, ListNode> hashMap = new HashMap<>();
-    hashMap.put(0, dummy);
-    for (ListNode i = dummy; i != null; i = i.next) {
-        prefixSum += i.val;
-        hashMap.put(prefixSum, i);
+    public ListNode removeZeroSumSublists(ListNode head) {
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode dummy = new ListNode(0);
+        map.put(0, dummy);
+        dummy.next = head;
+        int prefixSum = 0;
+        for(ListNode i=dummy;i!=null;i=i.next) {
+            prefixSum += i.val;
+            map.put(prefixSum, i);
+        }
+        prefixSum = 0;
+        for(ListNode i=dummy;i!=null;i=i.next) {
+            prefixSum += i.val;
+            i.next = map.get(prefixSum).next;
+        }
+        return dummy.next;
     }
-    prefixSum = 0;
-    for (ListNode i = dummy; i != null; i = i.next) {
-        prefixSum += i.val;
-        i.next = hashMap.get(prefixSum).next;
-    }
-    return dummy.next;
-}
-    
 }
