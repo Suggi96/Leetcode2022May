386676dee136d1cdcfae950d4e44ff08d695ef12
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-15 11:42:27 +0530

    Time: 73 ms (38.70%), Space: 67.6 MB (26.17%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,17 +1,17 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        Map<String, Integer> memo = new HashMap<>();
         int ans = 0;
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Map<String, Integer> dp = new HashMap<>();
         for(String word: words) {
-            int longestChainWithCurrentWord = 0;
-            //generate all combinations of word by deleting a single char in word 
+            int best = 0;
             for(int i=0;i<word.length();i++) {
-                String predecessor  = word.substring(0, i) + word.substring(i+1, word.length());
-                longestChainWithCurrentWord = Math.max(longestChainWithCurrentWord, memo.getOrDefault(predecessor, 0) +1);
+                String pred = word.substring(0, i) + word.substring(i+1, word.length());
+                int val = dp.getOrDefault(pred, 0);
+                best = Math.max(best, val+1);
             }
-            memo.put(word, longestChainWithCurrentWord);
-            ans = Math.max(ans, longestChainWithCurrentWord);
+            dp.put(word, best);
+            ans = Math.max(ans, best);
         }
         return ans;
     }
