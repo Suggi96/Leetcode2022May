be61744a581f05e0bfd2ac71d5fa19afa0ae3aac
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-17 17:27:13 +0530

    Time: 7 ms (19.65%), Space: 53.7 MB (62.84%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -5,23 +5,28 @@ class Solution {
         boolean[] visited = new boolean[n];
         for(int i=0;i<n;i++) {
             if(!visited[i]) {
-                dfs(isConnected, i, visited);
+                bfs(isConnected, i, visited);
                 count++;
             }
         }
         return count;
     }
-    private void dfs(int[][] isConnected, int curVertex, boolean[] visited) {
-        if(visited[curVertex])
-            return;
+    private void bfs(int[][] isConnected, int curVertex, boolean[] visited) {
+      Queue<Integer> q = new LinkedList<>();
+      q.add(curVertex);
+      while(!q.isEmpty()) {
+          int curNode = q.remove();
+          visited[curNode] = true;
+          int[] arr = isConnected[curNode];
+          for(int i=0;i<arr.length;i++) {
+              if(visited[i])
+                  continue;
+              
+              if(arr[i]==1)
+                  q.add(i);
+          }
+      }
         
-        visited[curVertex] = true;
-        int[] neigh = isConnected[curVertex];
-        for(int i=0;i<neigh.length;i++) {
-            if(neigh[i]==1)
-                dfs(isConnected, i, visited);
-        }
-        return;
     }
     
 }
