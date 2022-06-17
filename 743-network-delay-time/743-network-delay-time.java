e85c995714b80887d5a695f563fe8140299898f1
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-17 09:32:35 +0530

    Time: 66 ms (11.92%), Space: 71.9 MB (5.08%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,62 +1,53 @@
 class Solution {
-
-	private class Node{
-		private int target;
-		private int distance;
-
-		public Node(int target, int dist){
-			this.target = target;
-			this.distance = dist;
-		}
-
-	}
-	public int networkDelayTime(int[][] times, int N, int K) {
-		// Min heap
-		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b) -> (a.distance - b.distance));
-
-		// Source Id, Connections that exist in the graph
-		Map<Integer, Set<Node>>  map = buildGraph(times);
-		Set<Integer> visited =  new HashSet<>();
-
-		pq.offer(new Node(K, 0));
-
-		int totalTime = 0;
-		while(!pq.isEmpty()){
-			int size = pq.size();
-			while(size-->0){
-				Node head = pq.poll();
-
-				if(visited.contains(head.target)){
-					continue;
-				}
-				visited.add(head.target);
-
-				totalTime = Math.max(totalTime, head.distance);
-
-
-				if(visited.size() == N){
-					return totalTime;
-				}
-				if(map.containsKey(head.target))
-					for(Node children: map.get(head.target)){
-						int childTime = children.distance + head.distance;
-						pq.offer(new Node(children.target, childTime));
-					}
-			}
-
-
-		}
-		return -1;
-	}
-
-
-	private Map<Integer, Set<Node>> buildGraph(int[][] times){
-		Map<Integer, Set<Node>> map = new HashMap<>();
-		for(int[] time: times){
-			map.putIfAbsent(time[0], new HashSet<>());
-			map.get(time[0]).add(new Node(time[1] ,time[2]));
-
-		}
-		return map;
-	}
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<ArrayList<Integer>>> graph = buildgraph(times, n);
+        //minheap to maintain vertices and its corresponding time
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>( (a, b) -> a.getValue() - b.getValue());
+        int[] distance = new int[n+1];
+        Arrays.fill(distance, -1);
+        pq.add(new Pair(k, 0));
+        while(!pq.isEmpty()) {
+            Pair<Integer, Integer> curPair = pq.remove();
+            int curVertex = curPair.getKey();
+            int curTime = curPair.getValue();
+            
+            if(distance[curVertex]!=-1)
+                continue;
+            
+            distance[curVertex] = curTime;
+            ArrayList<ArrayList<Integer>> curNeigh = graph.get(curVertex);
+            for(ArrayList<Integer> current: curNeigh) {
+                int adjNeigh = current.get(0);
+                int time = current.get(1);
+                pq.add(new Pair(adjNeigh, curTime + time));
+            }
+            
+        }
+        int reqTime = Integer.MIN_VALUE;
+        for(int i=1;i<=n;i++) {
+            if(distance[i]==-1)
+                return -1;
+            else {
+                reqTime = Math.max(reqTime, distance[i]);
+            }
+        }
+        return reqTime;
+        
+    }
+    private ArrayList<ArrayList<ArrayList<Integer>>> buildgraph(int[][] times, int n) {
+        ArrayList<ArrayList<ArrayList<Integer>>> graph = new ArrayList<>();
+        for(int i=0;i<=n;i++)
+            graph.add(new ArrayList<ArrayList<Integer>>());
+        
+        for(int[] edge: times) {
+            int source = edge[0];
+            int dest = edge[1];
+            int time = edge[2];
+            ArrayList<Integer> neigh = new ArrayList<>();
+            neigh.add(dest);
+            neigh.add(time);
+            graph.get(source).add(neigh);
+        }
+        return graph;
+    }
 }
