1cd0b8e024280e1317832b65ebe0a0bc2961e956
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-18 09:56:06 +0530

    Time: 748 ms (21.72%), Space: 73.5 MB (91.64%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -0,0 +1,27 @@
+class WordFilter {
+    HashMap<String, Integer> map = null;
+    public WordFilter(String[] words) {
+        map = new HashMap<>();
+        int index = 0;
+        for(String word: words) {
+            for(int i=0;i<=word.length();i++) {
+                for(int j=0;j<=word.length();j++) {
+                    String prefixAndsuffix = word.substring(0, i) + "#" + word.substring(j);
+                    map.put(prefixAndsuffix, index);
+                }
+            }
+            index++;
+        }
+    }
+    
+    public int f(String prefix, String suffix) {
+        String queryString = prefix + "#" + suffix;
+        return map.getOrDefault(queryString, -1);
+    }
+}
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter obj = new WordFilter(words);
+ * int param_1 = obj.f(prefix,suffix);
+ */
