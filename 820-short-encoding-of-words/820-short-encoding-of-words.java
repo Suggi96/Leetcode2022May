67c0938f804dc48168782e097042212f8fba335a
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-20 11:50:35 +0530

    Time: 14 ms (98.16%), Space: 42.7 MB (93.25%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,25 +1,16 @@
 class Solution {
     public int minimumLengthEncoding(String[] words) {
-          HashSet<String> hashset = new HashSet<>();
-		
-		// Add all the words in the hashset
-        for(String word : words) {
-            hashset.add(word);
-        }
-        
-        for(String word : words) {
-			// Remove every suffix string from each word from the hashset except for the full word
-			// Because a word and one of its suffix string can be encoded into a single string
-            for(int i = word.length() - 1; i > 0; i--) {
-                String substring = word.substring(i, word.length());
-                if(hashset.contains(substring)) hashset.remove(substring);
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        //deleting only suffixes
+        for(String s: words) {
+            for(int i=1;i<s.length();i++) {
+                set.remove(s.substring(i));
             }
         }
-        
         int length = 0;
-        for(String word : hashset) length += word.length();
-		
-		// length for character count and hashset size for the required number of #
-        return length + hashset.size();
+        for(String s: set)
+            length += s.length() + 1;
+        
+        return length;
     }
 }
