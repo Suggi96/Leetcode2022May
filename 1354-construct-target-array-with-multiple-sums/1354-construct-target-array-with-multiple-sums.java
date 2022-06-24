37ba1350d25648257cbb67f458300d8edf151d71
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-24 11:18:46 +0530

    Time: 18 ms (23.23%), Space: 57.2 MB (79.80%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,30 +1,25 @@
 class Solution {
     public boolean isPossible(int[] target) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a); //maxHeap
-        long totalSum = 0l;
-        for(int el: target) {
-            pq.offer(el);
-            totalSum += el;
-        }
-        while(!pq.isEmpty()) {
-            int maxEl = pq.poll();
-            long remainingTotal = totalSum - maxEl;
-            
-            if(maxEl==1 || remainingTotal==1)
-                return true;
-            
-            if(remainingTotal==0 || maxEl<remainingTotal)
-                return false;
-            
-            int updatedMax = (int)(maxEl % remainingTotal);
-            if(updatedMax==0) 
-                return false;
-            
-            maxEl = updatedMax;
-            pq.offer(maxEl);
-            totalSum = updatedMax + remainingTotal;
-            
-        }
-        return true;
+	if (target.length == 1) return target[0] == 1;
+
+	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+	int sum = 0;
+	for (int t: target) {
+		pq.add(t);
+		sum += t;
+	}
+
+	while (pq.peek() != 1) {
+		int curr = pq.poll();
+		if (sum - curr == 1) return true;
+
+		int x = curr % (sum - curr);
+		sum = sum - curr + x;
+
+		if (x == 0 || x == curr) return false;
+		else pq.add(x);
+	}
+
+	return true;
     }
 }
