97c3f1c6b4eba3c0740a2397e1bd75d644c234b8
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-27 11:23:36 +0530

    Time: 3 ms (22.22%), Space: 42.7 MB (33.33%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -1,13 +1,13 @@
 class Solution {
     public int countAsterisks(String s) {
-        boolean pairAst = true;
+        boolean pairAst = false;
         int count = 0;
         for(int i=0;i<s.length();i++) {
             char c = s.charAt(i);
             if(c=='|') {
                 pairAst = !pairAst;
             }
-            if(c=='*' &&  pairAst==true) 
+            if(c=='*' &&  pairAst==false) 
                 count++;
         }
         return count;
