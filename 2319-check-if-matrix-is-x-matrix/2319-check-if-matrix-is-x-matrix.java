7f410a76b0abce310d05da80919e9c53031bc5e2
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-28 09:44:40 +0530

    Time: 2 ms (92.22%), Space: 53.8 MB (86.58%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -1,30 +1,15 @@
 class Solution {
     public boolean checkXMatrix(int[][] grid) {
         int n = grid.length;
-        int r = 0, c = 0;
-        while(r<n && c<n) {
-            if(grid[r][c]==0) return false;
-            grid[r][c] = 0;
-            r++;
-            c++;
-        }
-        r =  0;
-        c = n-1;
-        while(r<n && c>=0) {
-            if(r==c &&  grid[r][c]==0) {
-                r++;
-                c--; 
-                continue;
-            }
-            if(grid[r][c]==0) return false;
-            grid[r][c] = 0;
-            r++;
-            c--;
-        }
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
-                if(grid[i][j]!=0) return false;
-            } 
+                if(i==j || i+j==n-1) {
+                    if(grid[i][j]==0) return false;
+                }
+                else {
+                    if(grid[i][j]!=0) return false;
+                }
+            }
         }
         return true;
     }
