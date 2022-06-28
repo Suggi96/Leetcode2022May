c039a04e7d33340c2fa6dbdf310b01766b0a39ea
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-06-28 19:40:05 +0530

    Time: 115 ms (41.87%), Space: 200.2 MB (28.94%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<n;i++)
+            graph.add(new ArrayList<>());
+        for(int[] e: edges) {
+            int u = e[0];
+            int v = e[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        boolean[] visited = new boolean[n];
+        long res=0; int count[]={0}; int prev=0;
+        for(int v=0;v<n;v++) {
+            if(!visited[v]) {
+                dfs(graph, v, visited, count);
+                long a = n - count[0];
+                long b = count[0] - prev;
+                prev = count[0];
+                res += (a*b);
+            }
+        }
+        return res;
+    }
+    private void dfs(ArrayList<ArrayList<Integer>> graph, int curVertex, boolean[] visited, int[] nodes) {
+        visited[curVertex] = true;
+        List<Integer> neigh = graph.get(curVertex);
+        nodes[0]++;
+        for(int n: neigh) {
+            if(!visited[n]) 
+                dfs(graph, n, visited, nodes);
+        }
+    }
+}
