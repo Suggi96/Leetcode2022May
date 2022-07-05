d3a7574ae4c5f7330cbe88aec017552bc1a806b5
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-05 11:44:17 +0530

    Time: 1 ms (87.69%), Space: 42.4 MB (27.50%) - LeetHub

--- 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.java
+++ 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.java
@@ -1,15 +1,9 @@
 class Solution {
     public long[] sumOfThree(long num) {
-        long[] ans = new long[3];
-        if(num==0) {
-            return new long[]{-1,0,1};
-        }
-        if(num%3!=0)
-            return new long[]{};
-        
-        
-        long n = 1 + (num/3);
-        return new long[]{n-2,n-1,n};
-        
+	if (num % 3 != 0) {
+		return new long[0];
+	}
+	long a = num / 3 - 1;
+	return new long[] {a, a + 1, a + 2};
     }
 }
