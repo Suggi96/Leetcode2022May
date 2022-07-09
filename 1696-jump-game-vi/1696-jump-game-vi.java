fb183e58309469d4ba3d1a1f31d1962659e84df6
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-09 13:26:38 +0530

    Time: 150 ms (10.62%), Space: 112.9 MB (40.14%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -6,16 +6,16 @@ class Solution {
         //score, index
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (b[0]- a[0])); //create max heap based on score value b[0] - a[0]
         pq.add(new int[] {nums[0], 0}); //add first element of nums array
-        int max = nums[0];
+        int maxScore = nums[0];
         for(int i=1;i<n;i++) {
-            while(pq.peek()[1]<i-k) {
+            while(!(i-pq.peek()[1] <= k )) {
                 pq.poll();
             }
             int[] cur = pq.peek();
-            max = cur[0] + nums[i];
-            pq.offer(new int[] {max, i});
+            maxScore = cur[0] + nums[i];
+            pq.offer(new int[] {maxScore, i});
         }
-        return max;
+        return maxScore;
         
     }
 }
