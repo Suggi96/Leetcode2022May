0a61d6bdfc315a26ba53e77d41c5c55e0e38ab0d
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-12 10:52:33 +0530

    Time: 351 ms (13.47%), Space: 41.6 MB (75.39%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,28 +1,45 @@
 class Solution {
-    public boolean makesquare(int[] ms) {
-        int sum = 0;
-        for(int i: ms)
-            sum += i;
-        if(sum%4!=0) return false;
-        boolean[] visited = new boolean[ms.length];
-        return backtrack(ms, 4, visited, 0, 0, sum/4);
+    public boolean makesquare(int[] nums) {
+        if (nums.length < 4) return false;
+
+        int perimeter = 0;
+        for (int el : nums)  {
+            perimeter += el;
+        }
+
+        if (perimeter % 4 != 0) return false;
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=0;i<n/2;i++) {
+            int t = nums[i];
+            nums[i] = nums[n-1-i];
+            nums[n-1-i] = t;
+        }
+        int side = perimeter / 4;
+        int[]  sides = new int[] {side, side, side, side};
+
+        return makesquareHelper(nums, 0, sides);
     }
-    private boolean backtrack(int[] ms, int subsets, boolean[] visited, int curIdx, int curSum, int targetSum) {
-        if(subsets==0)
-            return true;
-        if(curSum>targetSum)
-            return false;
-        if(curSum==targetSum)
-            return backtrack(ms, subsets-1, visited, 0, 0, targetSum);
-        
-        for(int i=curIdx;i<ms.length;i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                if(backtrack(ms, subsets, visited, i+1, curSum+ms[i], targetSum))
-                    return true;
-                visited[i] = false;
+
+    private boolean makesquareHelper(int[] nums, int i, int[] sides) {
+        if(i == nums.length) {
+
+            if(sides[0] == 0 && sides[1] == 0 && sides[2] == 0 && sides[3] == 0){
+                return true;
+            } else{
+                return false;
             }
         }
+
+
+        for (int j = 0; j < 4; j++) {
+            if (nums[i] > sides[j]) continue;
+            sides[j] -= nums[i];
+            if (makesquareHelper(nums, i + 1, sides)) return true;
+            sides[j] += nums[i];
+        }
+
         return false;
     }
 }
