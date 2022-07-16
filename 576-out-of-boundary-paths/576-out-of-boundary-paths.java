68932984a8208dc1939a53cc0d4b251d5e5fb80c
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-16 11:15:16 +0530

    Time: 5 ms (92.71%), Space: 41.2 MB (96.04%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -1,27 +1,27 @@
 class Solution {
     int mod = (int)Math.pow(10,9) + 7;
+    Long[][][] memo = null;
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        HashMap<String,Long> memo = new HashMap<>();
-        return (int)dfs(m,n,maxMove,startRow,startColumn, memo);
+        memo = new Long[m+1][n+1][maxMove+1];
+        return (int)dfs(m,n,maxMove,startRow,startColumn);
     }
-    private long dfs(int m, int n, int maxMove, int i, int j, HashMap<String,Long> memo) {
+    private long dfs(int m, int n, int maxMove, int i, int j) {
         if(i<0 || i>=m || j<0 || j>=n)
             return 1;
         
         if(maxMove<=0)
             return 0;
         
-        String key = maxMove + "-" + i + "-" + j + "";
-        if(memo.containsKey(key))
-            return memo.get(key);
+        if(memo[i][j][maxMove]!=null)
+            return memo[i][j][maxMove];
         
         long res = 0;
-        res += dfs(m, n, maxMove-1, i, j+1, memo);
-        res += dfs(m, n, maxMove-1, i+1, j, memo);
-        res += dfs(m, n, maxMove-1, i-1, j, memo);
-        res += dfs(m, n, maxMove-1, i, j-1, memo);
+        res += dfs(m, n, maxMove-1, i, j+1);
+        res += dfs(m, n, maxMove-1, i+1, j);
+        res += dfs(m, n, maxMove-1, i-1, j);
+        res += dfs(m, n, maxMove-1, i, j-1);
         
-        memo.put(key, res%mod);
-        return memo.get(key);
+        memo[i][j][maxMove] = res % mod;
+        return res % mod;
     }
 }
