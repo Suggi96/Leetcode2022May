f3eabd2feae9304db1bec0ca31b2ad733a7f2759
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-16 10:56:12 +0530

    Time: 97 ms (5.42%), Space: 55 MB (5.63%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+    int mod = (int)Math.pow(10,9) + 7;
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        HashMap<String,Long> memo = new HashMap<>();
+        return (int)dfs(m,n,maxMove,startRow,startColumn, memo);
+    }
+    private long dfs(int m, int n, int maxMove, int i, int j, HashMap<String,Long> memo) {
+        if(i<0 || i>=m || j<0 || j>=n)
+            return 1;
+        
+        if(maxMove<=0)
+            return 0;
+        
+        String key = maxMove + "-" + i + "-" + j + "";
+        if(memo.containsKey(key))
+            return memo.get(key);
+        
+        long res = 0;
+        res += dfs(m, n, maxMove-1, i, j+1, memo)%mod;
+        res += dfs(m, n, maxMove-1, i+1, j, memo)%mod;
+        res += dfs(m, n, maxMove-1, i-1, j, memo)%mod;
+        res += dfs(m, n, maxMove-1, i, j-1, memo)%mod;
+        
+        memo.put(key, res%mod);
+        return memo.get(key);
+    }
+}
