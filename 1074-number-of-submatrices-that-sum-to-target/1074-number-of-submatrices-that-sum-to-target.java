7c18d07427a71aa3c8df7b9247739540b68b2dfb
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-18 12:23:30 +0530

    Time: 270 ms (46.90%), Space: 117 MB (61.54%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int numSubmatrixSumTarget(int[][] A, int target) {
-                int res = 0, m = A.length, n = A[0].length;
-        for (int i = 0; i < m; i++)
-            for (int j = 1; j < n; j++)
-                A[i][j] += A[i][j - 1];
-        Map<Integer, Integer> counter = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                counter.clear();
-                counter.put(0, 1);
-                int cur = 0;
-                for (int k = 0; k < m; k++) {
-                    cur += A[k][j] - (i > 0 ? A[k][i - 1] : 0);
-                    res += counter.getOrDefault(cur - target, 0);
-                    counter.put(cur, counter.getOrDefault(cur, 0) + 1);
+public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] array = new int[m];
+        int result = 0;
+        for(int i = 0; i < n; i++) { //i is the starting column
+            Arrays.fill(array, 0);
+            for(int j = i; j < n; j++) { //j is the ending column
+                for(int k = 0; k < m; k++) {
+                    array[k] += matrix[k][j];
                 }
+                result += subarraySum(array, target);
             }
         }
-        return res;
+        return result;
+    }
+    
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return result;
     }
 }
