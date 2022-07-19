eba6487bff0956a3b2709a57e4177857e75c0e45
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-07-19 12:34:05 +0530

    Time: 7 ms (5.39%), Space: 43 MB (6.11%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countElements(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        if(n<=2) return 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int s = 0, e = n-1;
+        while(s+1<n && nums[s]==nums[s+1]) s++;
+        while(e-1>=0 && nums[e-1]==nums[e]) e--;
+        int count = 0;
+        for(int i=s+1;i<e;i++) 
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        for(Map.Entry<Integer, Integer> en: map.entrySet()) {
+            count += en.getValue();
+        }
+        return count;
+        
+    }
+}
